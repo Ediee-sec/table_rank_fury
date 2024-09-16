d0e64acdd1ea138c1f4c49512f9bc1e9edb8b7d4
Ediee-sec <emersonrox8@gmail.com>
2024-09-15 21:55:38 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from models import db, User
+import os
+
+app = Flask(__name__)
+
+# Configurando a conexão com o PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv('USR')}:{os.getenv("PASS")}@{os.getenv("HOST")}:{os.getenv("PORT")}/{os.getenv("DB")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+# Rota principal
+@app.route('/')
+def index():
+    # Consulta todos os usuários da tabela e ordena pelo campo 'level' (lvl)
+    users = User.query.order_by(User.xp_accumulated.desc()).all()  # Ordena por nível em ordem decrescente
+    return render_template('table.html', users=users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'rank'
+    user_id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.String(255), nullable=False)
+    user_dc = db.Column(db.String(100), nullable=False)
+    xp = db.Column(db.Integer, nullable=False)
+    xp_accumulated = db.Column(db.Integer, nullable=False)
+    lvl = db.Column(db.Integer, nullable=False)
+    timer = db.Column(db.DateTime, nullable=False)
+    server_id = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, avatar_url, username, points_1, points_2, status):
+        self.avatar_url = avatar_url
+        self.username = username
+        self.points_1 = points_1
+        self.points_2 = points_2
+        self.status = status
