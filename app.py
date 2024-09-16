460f7d48efde3c2b639cf0a435a0d23daba06a68
Ediee-sec <emersonrox8@gmail.com>
2024-09-15 22:06:38 -0300

    update port flask

--- app.py
+++ app.py
@@ -18,4 +18,4 @@ def index():
     return render_template('table.html', users=users)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_debugger=True, use_reloader=False)
